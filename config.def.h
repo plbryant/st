9438aef1224e0b16eaea71b9b04cd9a757b68804
Bryant Paz <pazbryant7@outlook.com>
2024-06-02 20:46:02 -0600

    chore: update custom colors

--- config.def.h
+++ config.def.h
@@ -118,27 +118,29 @@ float alpha = 1.0;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-  "#3e353b",
-  "#d4969c",
-  "#b1a1d1",
-  "#e9cdd0",
-  "#70a9a1",
-  "#b58da6",
-  "#75a7b3",
-  "#c5bdd2",
-  "#4c566a",
-  "#d8dee9",
-  "#eceff4",
-  "#5e81ac",
-  "#434c5e",
-  "#d08770",
-  "#8fbcbb",
-  "#e9cdd0",
-  [255] = 0,
-  "#c7aed6",
-  "#3d353b",
-  "#191724",
-  "#c7aed6",
+	/* 8 normal colors */
+	"#51576D",
+	"#E78284",
+	"#A6D189",
+	"#E5C890",
+	"#8CAAEE",
+	"#F4B8E4",
+	"#81C8BE",
+	"#B5BFE2",
+
+	/* 8 bright colors */
+	"#626880",
+	"#E78284",
+	"#A6D189",
+	"#E5C890",
+	"#8CAAEE",
+	"#F4B8E4",
+	"#81C8BE",
+	"#A5ADCE",
+
+  [256] = "#C6D0F5", /* default foreground colour */
+  [257] = "#303446", /* default background colour */
+  [258] = "#F2D5CF", /*575268*/
 };
 
 
@@ -146,10 +148,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 259;
-unsigned int defaultbg = 258;
-unsigned int defaultcs = 256;
-unsigned int defaultrcs = 257;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 258;
 
 /*
  * https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-Ps-SP-q.1D81
